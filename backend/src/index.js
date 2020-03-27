6219ae1dbfa7d0865493d43c7d659663fcec72c9
xbergx <berg215@hotmail.com>
2020-03-27 18:27:11 -0300

    back-end da aplicaçao

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+const express = require ('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express ();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP:
+  * Insominia: Testar as rotas , GET ,POST , PUT, DELETE ...
+  * GET: Buscas / listar uma informação do back-end
+  * POST: Criar uma informação no back-end
+  * PUT: Alterar uma informação no bak-end
+  * DELETE: Deletar uma informação no back-end
+  */
+
+  /**
+   * Tipos de parâmetros:
+   * 
+   * Query params: Parâmetros nomeados enviados na rota "?" (Filtros, paginação)
+   * Route params: Parâmetros utilizados para identificar recursos
+   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+   * 
+   */
+
+
+
